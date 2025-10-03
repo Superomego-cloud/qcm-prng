e441598a7fd81b1be4359596303d023694e6e93d
superomego <Elyas>
2025-10-03 16:59:30 +0200

    J'ai ajouté des précisions sur quelques fonctions et sur la manière d'utilisation du code

--- affichage.py
+++ affichage.py
@@ -3,7 +3,15 @@ from interne import *
 # Utilise la fonction input() pour prendre l'entrée de l'utilisateur
 # N'oublie pas de convertir le type après
 
-def afficher_questionnaire(q : list[int]):
+def afficher_questionnaire(q : list[int]) -> bool:
     """
-    Il s'agit de la fonction principale qui affiche le code.
+    Il s'agit de la fonction principale qui affiche le code. Elle doit être divisée en trois parties: 
+    
+    La première pour demander à l'utilisateur le fichier .txt à lire et le mode de notation (préciser que 
+    ce doit être un fichier .txt
+
+    La deuxième affiche les questions une à la fois tant qu'il y a des questions
+
+    La dernière affiche la note finale de l'utilisateur, et lui demande s'il veut continuer ou non. Si
+    oui, la fonction retourne True, sinon elle retourne False
     """
--- interne.py
+++ interne.py
@@ -1,8 +1,20 @@
+"""
+Base de donnée interne du QCM
+Yvan Van de Steene
+"""
+
 from qcm, prng_it import *
 
-# cette variable est à remplir, elle est la base de données 
+# cette variable est à remplir, elle est la base de données
+# Les variables définies ici peuvent être utilisés par les fonctions
 database : list
-# ce n'est pas la seule variable, il y a d'autres à faire pour la notation, pour les questions...
+
+
+def initialiser_donnees(entree: str) -> None:
+    """
+    Cette fonction prend le nom du fichier et initialise les données du questionnaires, destinéees
+    à utilisation par les autres fonctions
+    """
 
 def traiter_question(entree: str) -> tuple(bool, str):
     """
@@ -17,5 +29,8 @@ def question_suivante() -> list[int]:
 
 def note_finale() -> tuple[int | None, int | None]:
     """
-    Cette fonction sera appelée par l'élément d'affichage dès que le questionnaire est terminé pour donner la note finale avec les deux systèmes de notation. La première valeur est la notation normale, la deuxième est la notation sévère. Si les paramètres de l'utilisateur font qu'il veuille qu'un des deux, l'autre valeur sera None
+    Cette fonction sera appelée par l'élément d'affichage dès que le questionnaire est terminé pour donner la 
+    note finale avec les deux systèmes de notation. La première valeur est la notation normale, la deuxième est 
+    la notation sévère. Si les paramètres de l'utilisateur font qu'il veuille qu'un des deux, l'autre valeur doit 
+    être None
     """
