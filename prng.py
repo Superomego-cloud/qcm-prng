de59e4f881ab953ce7e9c846bab6bd39cf9dc545
superomego <Elyas>
2025-10-03 17:15:41 +0200

    Ecriture de la structure de mon fichier

--- prng.py
+++ prng.py
@@ -1,2 +1,28 @@
+"""
+Implémentation de PRNG
+Elyas Sahnoune
+"""
+
+import math
+
+graine: int
+
+def generer_nombre(a: int, b: int) -> int:
+
+    """
+    Fonction qui génère un nombre aléatoire compris entre a et b 
+    """
+
 def generer_permutation(entree:list) -> list:
-    
+    """
+    Fonction qui prend une liste mutable d'éléments et qui retourne une liste
+    contenant les mêmes éléments que le paramètres mais pas nécéssairement dans
+    le même ordre
+    """
+
+def tester_prng(constr: type, nbr_iter: int = 100_000, nbr_graines: int = 10) -> bool:
+    """
+    Fonction qui prend un constructeur de PRNG, un nombre d'itérations, et un nombre de graines à tester, et retourne une booléene qui dit si l'implémentation est correcte ou non
+    """
+
+
